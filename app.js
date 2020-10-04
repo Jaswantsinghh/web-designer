a34afef9ed3a340cda87ae21c355e06485e3672d
Jaswantsinghh <jaswantsinghjsn@gmail.com>
2020-10-05 01:40:49 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function myFunc()
+{
+let x=document.getElementById("col").value;
+document.body.style.backgroundColor=x;
+}
+function mySlide()
+{
+    let a=document.getElementById("slide").value;
+    final = a+"px";
+    document.getElementById("h").style.fontSize = final;
+}
